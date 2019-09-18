a7a661f34c9a5dc6102655e0463770dec4b92dbe
ahmad4hat <rappar.stand@gmail.com>
2019-09-19 01:32:02 +0600

    Added authoraization campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -44,25 +44,19 @@ router.post("/", isLoggedin,function (req, res) {
 //edit campground route
 
 
-router.get("/:id/edit",(req,res)=>{
-        Campground.findById(req.params.id,(err,campground)=>{
-           
-            if(err)
-            {
-                console.log(err);
-            }
-            else
-            {
-                res.render("campgrounds/edit",{campground});
-            }
-        })
-        
+router.get("/:id/edit",campgroundAuthenticator,(req,res)=>{
+        Campground.findById(req.params.id,(err,campground)=>{            
+                if(campground.author.id.equals(req.user._id)){
+                    res.render("campgrounds/edit",{campground});
+                }
+          
+        });        
 }) 
 
 //update campground route
 
 
-router.put("/:id",(req,res)=>{
+router.put("/:id",campgroundAuthenticator,(req,res)=>{
     Campground.findOneAndUpdate({_id:req.params.id},{name:req.body.name,image:req.body.image,description:req.body.description},(err,campground)=>{
         if(err)
         {
@@ -87,7 +81,7 @@ router.put("/:id",(req,res)=>{
 //     }
 
 // })
-router.delete("/:id",(req,res)=>{
+router.delete("/:id",campgroundAuthenticator,(req,res)=>{
     Campground.findByIdAndRemove(req.params.id,(err)=>{
         if(err)
         {
@@ -146,4 +140,36 @@ function isLoggedin(req,res,next){
     res.redirect("/login");
 
 }
+
+function campgroundAuthenticator(req,res,next)
+{
+    if(req.isAuthenticated())
+    {
+        Campground.findById(req.params.id,(err,campground)=>{
+           
+            if(err)
+            {
+                console.log(err);
+                res.redirect("back");
+            }
+            else
+            {
+                if(campground.author.id.equals(req.user._id)){
+                   next();
+                }
+                else
+                {
+                   res.redirect("back")
+                }
+                
+            }
+        });
+    }
+    else {
+        
+        res.redirect("back");
+    }
+
+}
+
 module.exports=router;
